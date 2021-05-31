a4a552d0929aea89d6975ee77413c9c3752c81e1
gonzoden <deniz.nazarova.99@gmail.com>
2021-05-31 12:07:43 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+app = Tk()
+app.title('Buguette')
+app.geometry('400x400')
+
+def bonjour():
+	bonjour = Label(app, text = "Bonjour, " + textBox.get())
+	bonjour.pack()
+
+text = Label(app, text="Bonjour! Comment tu t'appelles?")
+text.pack()
+
+textBox = Entry(app, width=15)
+textBox.pack()
+
+button = Button(app, text="On y va!", command = bonjour)
+button.pack()
+
+app.mainloop()
